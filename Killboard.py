2727f97c5612e5147d43d3cc992232bc5c5db734
nickup9 <nicholas.shpetner9@gmail.com>
2018-08-03 18:30:13 -0400

    Added a lot more selfs. While get_url() seems to be reconized as defined now, verify_url() does not reconize a arg (tgt) being passed to it, and breaks during init.

--- Killboard.py
+++ Killboard.py
@@ -7,9 +7,9 @@ class Killboard:
 	#TODO: Seperate into functions, add verification earlier in main.
 	def verify_URL(self, tgt):
 		#Status codes: 0 is ok, 1 is end of board, 2 is HTTP error
-		temp = requests.get(tgt)
+		temp = self.requests.get(tgt)
 		try:
-			temp.raise_for_status()
+			temp.self.raise_for_status()
 			#404/error check
 		except HTTPError:
 			return 2
@@ -22,25 +22,25 @@ class Killboard:
 	def pull_kills(self):
 		page = 1
 		#As per Zkill API docs, default page is 1.
-		print('Base URL: ' + url)
+		print('Base URL: ' + self.url)
 		print('Checking if killboard exists...')
-		temp_url = url
+		temp_url = self.url
 		while 1:	
 			temp_url = temp_url + 'page/' + str(page) + '/'
-			if verify_URL(temp_url) != 0:
+			if self.verify_URL(temp_url) != 0:
 				break
 			print('Now pulling from page' + page)
 			buff = requests.get(temp_url)
 			killbuffer = buff.json()
 			for x in killbuffer:
-					board[str(x['killmail_id'])] = x['zkb']['totalValue']
+					self.board[str(x['killmail_id'])] = x['zkb']['totalValue']
 			page = page + 1
 
 	def sort(self, modifer):
 		#modifer of 1 will make order decending.
-		board = sorted(board.items(), key=operator.itemgetter(1))
+		self.board = sorted(self.board.items(), key=operator.itemgetter(1))
 		if modifer == 1:
-			board.reverse()
+			self.board.reverse()
 		#From a academic standpoint, this is cheating, but it works. This actually turns kills into a list of tuples.
 		#TODO: Make actual fucking quicksort
 
@@ -48,16 +48,16 @@ class Killboard:
 		#TODO: Make this only a sorting function
 		#ID is a int, Type is 1 for corps, 2 for alliances
 		baseURL = 'https://zkillboard.com/api/zkbOnly/kills/'
-		if org_type == 1:
-			baseURL = baseURL + 'corporationID/' + str(org_ID) + '/'
-		elif org_type == 2:
-			baseURL = baseURL + 'allianceID/' + str(org_ID) + '/'
+		if self.org_type == 1:
+			baseURL = baseURL + 'corporationID/' + str(self.org_ID) + '/'
+		elif self.org_type == 2:
+			self.baseURL = baseURL + 'allianceID/' + str(self.org_ID) + '/'
 
 	def to_file(self):
 		final_url = 'http://zkillboard.com/kill/'
 		out = input("What should the new file be named?")
 		f = open(out, "w")
-		for x in board:
+		for x in self.board:
 			f.write('KILL ID: ' + str(x[0]) + ' | ' + 'ISK: ' + str(x[1]) + ' | URL: ' + final_url + str(x[0]) + '/ \n')
 		f.close()
 		print('Output file created as ' + out)
@@ -67,15 +67,15 @@ class Killboard:
 		self.org_type = 0
 		self.url = ''
 		while 1:
-			org_ID = int(input("\nEnter the ID of the organization, must be corp or alliance \n"))
+			self.org_ID = int(input("\nEnter the ID of the organization, must be corp or alliance \n"))
 			while 1:
-				org_type = int(input("\nEnter 1 if the organization is a corp, 2 if alliance \n"))
-				if org_type == 1 or org_type == 2:
+				self.org_type = int(input("\nEnter 1 if the organization is a corp, 2 if alliance \n"))
+				if self.org_type == 1 or self.org_type == 2:
 					break
 				else:
 					print("\nInvalid Identifier, try again\n")
-			url = get_url()
-			if verify_URL(url) == 0:
+			url = self.get_url()
+			if self.verify_URL(url) == 0:
 				break
 			else:
 				print('Invalid URL, from the top now...')
